13ea185c4c3887b64a2a9f432bcef261ae28330d
warchief11 <nardeep.chaudhary@gmail.com>
2016-06-05 02:46:45 +1000

    Added dummy model
    Added dummy model

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genreList = new List<Genre>
+                                {
+                                    new Genre { Name = "Rock" },
+                                    new Genre { Name = "Disco" },
+                                    new Genre { Name = "Jazz" }
+                                };
+            return View(genreList);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre {Name = genre };
+            return View(genreModel);
+        }
+
+        public ActionResult Details(string title)
+        {
+            var album = new Album { Title = title };
+            return View(album);
+        }
+    }
+}
---
+++ MusicStore/Models/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class Album
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Singer { get; set; }
+
+    }
+}
